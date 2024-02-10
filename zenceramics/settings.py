064604da7ac76e23e12c8a7f094fe113372692ef
emidombek <emilia.maria.dombek@gmail.com>
2024-02-10 17:42:54 +0000

    Add settings, requirements and models

--- zenceramics/settings.py
+++ zenceramics/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import sys
 import dj_database_url
 from pathlib import Path
 if os.path.isfile('env.py'):
