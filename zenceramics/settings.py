ce5d0de334dfc53da7613a0ec703a383e3b21007
emidombek <emilia.maria.dombek@gmail.com>
2024-02-10 17:46:08 +0000

    Update settings with allowed hosts

--- zenceramics/settings.py
+++ zenceramics/settings.py
@@ -32,7 +32,11 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [".herokuapp.com",]
+ALLOWED_HOSTS = [".herokuapp.com",
+    "localhost",
+    "127.0.0.1",
+    "127.0.0.1:8000",
+]
 
 SITE_ID = 1
 
