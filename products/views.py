b289cdaa54eb8289146c4fb02f77b8d1e7061fc3
emidombek <emilia.maria.dombek@gmail.com>
2024-02-18 15:58:03 +0000

    Add product detail view

--- products/views.py
+++ products/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic import DetailView
 from .models import Product
 
 class ProductListView(ListView):
     model = Product
     template_name = 'products/product_list.html'
     context_object_name = 'products'
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
--- zenceramics/urls.py
+++ zenceramics/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
-     path('', include("home.urls")),
+    path('', include("home.urls")),
+
 ]
+
