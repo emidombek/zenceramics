9cfa618826c9dd5790b5b47738460941bdf08371
emidombek <emilia.maria.dombek@gmail.com>
2024-02-18 17:06:48 +0000

    Add url to orders.py orders app

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import order_create, OrderDetailView, OrderListView
+from . import views
 
 urlpatterns = [
     path('create/', order_create, name='order_create'),
-    path('detail/<int:order_id>/', OrderDetailView.as_view(), name='order_detail'),
     path('my_orders/', OrderListView.as_view(), name='my_orders'),
+    path('order/<int:order_id>/', views.order_detail, name='order_detail'),
 ]
