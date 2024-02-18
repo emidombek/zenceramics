70283f17359ce849bd21a80130190ece43894caa
emidombek <emilia.maria.dombek@gmail.com>
2024-02-18 16:09:06 +0000

    Create admin model for orders app

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+    raw_id_fields = ['product']  # Helps to search for a product in a large dataset
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'order_date', 'total_price', 'status']
+    list_filter = ['status', 'order_date']
+    inlines = [OrderItemInline]
+
+admin.site.register(Order, OrderAdmin)
+
