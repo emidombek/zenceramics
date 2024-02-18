51518c42b8caa05b144d9b8f472bcf8196fe0a84
emidombek <emilia.maria.dombek@gmail.com>
2024-02-18 15:54:59 +0000

    Add admin model to product app and styling

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category', 'inventory', 'created_at', 'updated_at']
+    list_filter = ['category', 'created_at']
+    search_fields = ['name', 'category']
+
