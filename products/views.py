efc81578d06de8c1dfadf1cbdd367671dcced3b2
emidombek <emilia.maria.dombek@gmail.com>
2024-02-26 11:28:21 +0000

    Updating template path in views

--- products/views.py
+++ products/views.py
@@ -5,10 +5,10 @@ from .models import Product
 
 class ProductListView(ListView):
     model = Product
-    template_name = 'products/product_list.html'
+    template_name = 'product_list.html'
     context_object_name = 'products'
 
 class ProductDetailView(DetailView):
     model = Product
-    template_name = 'products/product_detail.html'
+    template_name = 'product_detail.html'
     context_object_name = 'product'
