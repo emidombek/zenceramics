742d891cb1dfa0f8eb420c35ca0a0321180e7504
emidombek <emilia.maria.dombek@gmail.com>
2024-03-10 22:38:19 +0000

    Add category links and views

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProductListView, ProductDetailView
+from .views import ProductListView, ProductDetailView, ProductListByCategoryView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('category/<str:category>/', ProductListByCategoryView.as_view(), name='product_list_by_category'),
 ]
--- products/views.py
+++ products/views.py
@@ -7,9 +7,18 @@ class ProductListView(ListView):
     model = Product
     template_name = 'product_list.html'
     context_object_name = 'products'
-    paginate_by = 10  # Display 10 products per page
+    paginate_by = 10
 
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'product_detail.html'
     context_object_name = 'product'
+
+class ProductListByCategoryView(ListView):
+    model = Product
+    template_name = 'products/product_list.html' 
+
+    def get_queryset(self):
+        """Filter products by category."""
+        category = self.kwargs.get('category')
+        return Product.objects.filter(category__iexact=category)
