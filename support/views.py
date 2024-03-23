7adcb491affed4973a9aa31448c1a8340c6f386d
emidombek <emilia.maria.dombek@gmail.com>
2024-03-23 12:54:21 +0000

    Add in contact template, views, form + url

--- support/views.py
+++ support/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.contrib import messages
 
 def about_view(request):
     return render(request, 'support/about.html') 
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()  # Save the contact message to the database
+            messages.success(request, 'Your message has been sent!')
+            return redirect('contact')  # Redirect to a new URL
+    else:
+        form = ContactForm()  # An unbound form
+
+    return render(request, 'support/contact.html', {'form': form})
