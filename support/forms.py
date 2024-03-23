d5cdb963313511a57beaca7ada9b97f938eeea32
emidombek <emilia.maria.dombek@gmail.com>
2024-03-23 17:24:32 +0000

    Add in form validation and errors

--- support/forms.py
+++ support/forms.py
@@ -1,7 +1,15 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Contact
-
+from django.core.validators import MaxLengthValidator
 class ContactForm(forms.ModelForm):
+    message = forms.CharField(widget=forms.Textarea, validators=[MaxLengthValidator(500)])
     class Meta:
         model = Contact
         fields = ['name', 'email', 'subject', 'message']
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        if len(name) < 2:
+            raise ValidationError("Your name must be at least 2 characters long.")
+        return name
--- support/views.py
+++ support/views.py
@@ -10,9 +10,10 @@ def contact_view(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()  # Save the contact message to the database
-            messages.success(request, 'Your message has been sent!')
-            return redirect('contact')  # Redirect to a new URL
+            messages.success(request, 'Your message has been sent! We will be in touch shortly.')
+            # Adjusted to use the namespaced URL
+            return redirect('support:contact')  # Redirect using the 'support' namespace
     else:
-        form = ContactForm()  # An unbound form
+        form = ContactForm()
 
     return render(request, 'support/contact.html', {'form': form})
