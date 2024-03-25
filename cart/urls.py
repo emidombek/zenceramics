4672c5de5179f36b6528b93d91131a37143fe714
emidombek <emilia.maria.dombek@gmail.com>
2024-03-25 12:23:14 +0000

    Add check url config and link

--- cart/urls.py
+++ cart/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.cart_view, name='cart_view'),
     path('cart/add/<int:product_id>/', views.add_to_cart_view, name='add_to_cart'),
     path('cart/remove/<int:product_id>/', views.remove_from_cart_view, name='remove_from_cart'),
-    path('cart/update/<int:product_id>/', views.update_cart_view, name='update_cart'),  
+    path('cart/update/<int:product_id>/', views.update_cart_view, name='update_cart'), 
+    path('checkout/', views.checkout_view, name='checkout'), 
 ]
