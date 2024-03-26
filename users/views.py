ad78cd5e3dde704f28f309e305ddc4b4fa2dce8d
emidombek <emilia.maria.dombek@gmail.com>
2024-03-26 20:34:47 +0000

    Add profile view and template

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Address, Wishlist
+from orders.models import Order
 
-# Create your views here.
+@login_required
+def profile_view(request):
+    user = request.user
+    orders = Order.objects.filter(user=user, status='Pending').order_by('-order_date')
+    addresses = Address.objects.filter(user=user)
+    wishlists = Wishlist.objects.filter(user=user)
+    
+    context = {
+        'orders': orders,
+        'addresses': addresses,
+        'wishlists': wishlists,
+    }
+    return render(request, 'user/profile.html', context)
+
