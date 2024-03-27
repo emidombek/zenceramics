1cae01e3ae038e8933a932e2a961c590362b0a23
emidombek <emilia.maria.dombek@gmail.com>
2024-03-27 22:07:01 +0000

    Add form for wishlist

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import Address
+from .models import Address,Wishlist
 
 class AddressForm(forms.ModelForm):
     class Meta:
         model = Address
         fields = ['name', 'address_line_1', 'address_line_2', 'city', 'state_province_region', 'postal_zip_code', 'country', 'phone', 'is_default']
+
+class WishlistForm(forms.ModelForm):
+    class Meta:
+        model = Wishlist
+        fields = ['product', 'notes', 'quantity']
+        widgets = {'notes': forms.Textarea(attrs={'cols': 40, 'rows': 5})}
