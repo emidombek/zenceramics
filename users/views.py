ad3a4a4b5bab53bf121b5b2e228868cfd8eb8d70
emidombek <emilia.maria.dombek@gmail.com>
2024-03-27 22:07:47 +0000

    Create wishlist view

--- users/views.py
+++ users/views.py
@@ -17,3 +17,22 @@ def profile_view(request):
     }
     return render(request, 'users/profile.html', context)
 
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Product, Wishlist
+from .forms import WishlistForm
+
+@login_required
+def add_to_wishlist(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = WishlistForm(request.POST)
+        if form.is_valid():
+            wishlist_item = form.save(commit=False)
+            wishlist_item.user = request.user
+            wishlist_item.product = product
+            wishlist_item.save()
+            return redirect('wishlist_detail') 
+    else:
+        form = WishlistForm()
+    return render(request, 'add_to_wishlist.html', {'form': form, 'product': product})
