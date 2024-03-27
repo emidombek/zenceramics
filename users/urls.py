d0cd029f3cd654397dd97ee4fd8d270268fd29d6
emidombek <emilia.maria.dombek@gmail.com>
2024-03-27 22:13:23 +0000

    Add urls to app level

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import add_to_wishlist, wishlist_detail
 
 urlpatterns = [
     path('profile/', views.profile_view, name='profile'),
+    path('wishlist/add/<int:product_id>/', add_to_wishlist, name='add_to_wishlist'),
+    path('wishlist/', wishlist_detail, name='wishlist_detail'),
 ]
